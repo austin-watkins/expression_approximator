f95e7e4aa57e16c2e31e690dad3da9aa632376fb
austin-watkins <watkins.austin@gmail.com>
2019-12-03 16:49:22 -0700

    data generator + data

---
+++ data_generator.py
@@ -0,0 +1,81 @@
+from sympy import *
+from random import randint
+
+
+def form_image(content, fix_size=True, path='data2/'):
+    """
+    Given a string (content) creates a picture that has that expression (evaluated in LaTex).
+    """
+    assert isinstance(content, str)
+
+    preamble = "\\documentclass{article} \n\
+                \\usepackage[paperheight=80px, paperwidth=110px]{geometry} \
+                \\pagenumbering{gobble} \
+                \\begin{document}\\"
+
+    file_name = content
+    if fix_size:
+        # latex = '.  \\scalebox{1.2}{$' + content + '$}.'
+        latex = 'I\\texttt{' + content + '}\\\\I'
+        yield preview(latex, output='png', viewer='file', filename=f'{path}{file_name}.png', preamble=preamble)
+    else:
+        latex = f'${content}$'
+        yield preview(latex, output='png', viewer='file', filename=f'{path}{file_name}.png')
+
+
+def random_addition(largest_possible_value=1000):
+    """
+    Given the largest possible value.
+    Generates a random addition expression and the value that it would be if evaluated.
+    Note that the largest size is inclusive.
+    """
+    # fixme figure out how to get the largest size
+    possible_size = len(str(largest_possible_value))
+
+    # Generate integers.
+    first_operand = randint(0, largest_possible_value)
+    second_operand = randint(0, largest_possible_value)
+
+    # Get answer.
+    result = first_operand + second_operand
+
+    # form string representation.
+    first_operand = str(first_operand)
+    second_operand = str(second_operand)
+    first_operand = '0' * (possible_size - len(first_operand)) + first_operand
+    second_operand = '0' * (possible_size - len(second_operand)) + second_operand
+
+    expression = f'{first_operand} + {second_operand}'
+    return expression, result
+
+
+def multi_threaded_printing(expressions):
+    """
+    Prints out expressions using as many cores as the CPU has.
+    """
+    from multiprocessing import cpu_count, Pool
+
+    try:
+        iter(expressions)
+    except TypeError:
+        raise TypeError('expressions must be iterable.')
+    cores = cpu_count()
+    pool = Pool(processes=cores)
+    pool.map(form_image, expressions)
+
+
+if __name__ == '__main__':
+    expressions = set()
+    number_of_expressions = 200
+    while len(expressions) < number_of_expressions:
+        question, answer = random_addition(999999)
+        expressions.update([question])
+
+    import time
+
+    t0 = time.time()
+    multi_threaded_printing(expressions)
+    delta = time.time() - t0
+    print(f'Printing took:\n'
+          f'\tTOTAL\t{delta:.3} seconds \n'
+          f'\tAVERAGE\t{number_of_expressions/ delta :.4} exp/second')
